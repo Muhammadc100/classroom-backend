1526da7a096103bf37ea884d0c43da4307f4859a
Muhammad Asad <muhammadasadazeem861@gmail.com>
2026-01-29 19:58:13 +0500

    implement GET all subjects route

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+import subjectRoutes from "./routes/subjects";
+
+const app = express();
+const PORT = 8000;
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}))
+
+app.use(express.json());
+
+app.use('/api/subjects', subjectRoutes)
+
+app.get("/", (req, res) => {
+  res.send("Hello, Welcome to the Classroom API!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
