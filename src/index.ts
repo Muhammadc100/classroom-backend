8b6e0ded545366d25a45a49b2610dc8319262086
Muhammad Asad <muhammadasadazeem861@gmail.com>
2026-02-06 20:06:58 +0500

    bug fixing

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import express from "express";
 import cors from "cors";
 import { toNodeHandler } from 'better-auth/node'
 import subjectRoutes from "./routes/subjects.js";
-import securityMiddleware from "./middleware/security";
-import { auth } from "./lib/auth";
+import securityMiddleware from "./middleware/security.js";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 const PORT = 8000;
